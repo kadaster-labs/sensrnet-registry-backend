b6eb49802e2667365cbbdf7693f1f7771335f9da
Florijn <wim.florijn@kadaster.nl>
2020-06-08 10:08:09 +0200

    Initial authentication setup.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,19 @@ import { OwnerCommandModule } from './commands/owner/owner.module';
 import { SensorCommandModule } from './commands/sensor/sensor.module';
 import { OwnerQueryModule } from './query/owner/owner.module';
 import { SensorQueryModule } from './query/sensor/sensor.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { AppController } from './app.controller';
 
 @Module({
+  controllers: [AppController],
   imports: [
     OwnerCommandModule,
     SensorCommandModule,
     OwnerQueryModule,
     SensorQueryModule,
+    AuthModule,
+    UsersModule,
   ],
 })
 
