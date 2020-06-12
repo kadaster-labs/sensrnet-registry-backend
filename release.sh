310ccee6d64e10d08d9667f5c0918d6053a2e1ba
andelm <2315396+marcvanandel@users.noreply.github.com>
2020-06-12 16:11:24 +0200

    fix release script with version

--- release.sh
+++ release.sh
@@ -10,23 +10,25 @@ git pull
 # bump version
 npm version patch
 
-PACKAGE_VERSION=$(node -pe "require('./package.json').version")
-echo "double check: $PACKAGE_VERSION"
+VERSION=$(node -pe "require('./package.json').version")
+echo "version: $VERSION"
 
-version="$PACKAGE_VERSION"
-echo "version: $version"
+rm VERSION
+echo "$VERSION" >> VERSION
+
+exit 0
 
 # run build
 ./build.sh
 
 # tag it
 git add -A
-git commit -m "release v$version"
-git tag -a "$version" -m "release v$version"
-# git push
-# git push --tags
-docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$version
+git commit -m "release v$VERSION"
+git tag -a "$VERSION" -m "release v$VERSION"
+git push
+git push --tags
+docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$VERSION
 
 # push it
-# docker push sensrnetregistry.azurecr.io/$USERNAME/$IMAGE:latest
-# docker push sensrnetregistry.azurecr.io/$USERNAME/$IMAGE:$version
+docker push sensrnetregistry.azurecr.io/$USERNAME/$IMAGE:latest
+docker push sensrnetregistry.azurecr.io/$USERNAME/$IMAGE:$VERSION
