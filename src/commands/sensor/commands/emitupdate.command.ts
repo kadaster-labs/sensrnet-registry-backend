5b5941a312ec100cd5fb30f531848a65ec60b9da
Stefan Bussemaker <Stefan.Bussemaker@kadaster.nl>
2020-06-15 14:11:51 +0200

    reverse some oopsie

--- src/commands/sensor/commands/emitupdate.command.ts
+++
@@ -1,7 +0,0 @@
-import { ICommand } from '@nestjs/cqrs';
-
-export class EmitUpdateCommand implements ICommand {
-    constructor(public readonly heroId: string, public readonly itemId: string) {
-        console.log('new emitupdatecommand');
-    }
-}
