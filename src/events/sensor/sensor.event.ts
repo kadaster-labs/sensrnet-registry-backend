e8a607149a91d76fe00a76f6bb0543781394da24
Florijn <wim.florijn@kadaster.nl>
2020-06-24 19:26:18 +0200

    small formatting update

--- src/events/sensor/sensor.event.ts
+++ src/events/sensor/sensor.event.ts
@@ -5,12 +5,13 @@ const NODE_ID = process.env.NODE_ID || uuidv4();
 
 export abstract class SensorEvent extends Event {
 
+  readonly eventId: string;
   readonly sensorId: string;
   readonly nodeId: string = NODE_ID;
-  readonly eventId: string = uuidv4();
 
   constructor(sensorId: string) {
     super(sensorId);
+    this.eventId = uuidv4();
     this.sensorId = sensorId;
   }
 
