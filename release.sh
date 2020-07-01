a4e00c863c49d8f6f7a9eb14146e8d4dafa58406
Stefan Bussemaker <56066664+kad-busses@users.noreply.github.com>
2020-07-01 10:35:52 +0200

    fix double release tag (#34)

--- release.sh
+++ release.sh
@@ -13,7 +13,7 @@ az acr login --name sensrnetregistry
 # ensure we're up to date
 git pull
 # bump version
-npm version patch
+npm version patch --no-git-tag-version
 
 VERSION=$(node -pe "require('./package.json').version")
 echo "version: $VERSION"
