96ecadafd997d9d82a7b53692af48f3ce3d8cafa
Stefan Bussemaker <Stefan.Bussemaker@kadaster.nl>
2020-11-12 10:00:31 +0100

    add health endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TerminusModule } from '@nestjs/terminus';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { QueryModule } from './query/query.module';
 import { CommandModule } from './command/command.module';
 import { CheckpointModule } from './query/service/checkpoint/checkpoint.module';
+import { HealthController } from './health/health.controller';
 
 const port = process.env.MONGO_PORT || 27017;
 const host = process.env.MONGO_HOST || 'localhost';
@@ -18,7 +20,9 @@ const database = process.env.MONGO_DATABASE || 'sensrnet';
         CommandModule,
         CheckpointModule,
         MongooseModule.forRoot(`mongodb://${host}:${port}/${database}`),
+        TerminusModule,
     ],
+    controllers: [HealthController],
 })
 
 export class AppModule {}
