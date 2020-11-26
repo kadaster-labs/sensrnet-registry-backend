3e22a32571ce574ee8224d92bf4a091ba93106e0
Stefan Bussemaker <56066664+kad-busses@users.noreply.github.com>
2020-11-26 13:45:08 +0100

    K8s deployment improvements (#70)
    * add health endpoint
    
    * update k8s config files
    
    * Docker perform clean install for CI
    
    * load secret from k8s secret
    
    * fix tests
    
    * removed NODE_ID overlays

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ FROM node:12.18.4 AS builder
 WORKDIR /app
 
 COPY ./package*.json ./
-RUN npm install
+RUN npm ci
 
 COPY tsconfig*.json ./
 COPY src src
---
+++ src/health/health.controller.spec.ts
@@ -0,0 +1,25 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { HealthCheckService } from '@nestjs/terminus';
+import { HealthController } from './health.controller';
+
+class HealthCheckServiceMock {}
+
+describe('Health Controller', () => {
+  let controller: HealthController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [HealthController],
+      providers: [{
+        provide: HealthCheckService,
+        useClass: HealthCheckServiceMock,
+      }],
+    }).compile();
+
+    controller = module.get<HealthController>(HealthController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
