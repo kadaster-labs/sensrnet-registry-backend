698a941c1efda16d1eb78ab2a853262c274b1d9a
marcvanandel <marc.vanandel@kadaster.nl>
2021-03-09 19:59:37 +0100

    (re)located at base object events

--- src/core/events/sensordevice/device/index.ts
+++ src/core/events/sensordevice/device/index.ts
@@ -1,5 +1,8 @@
 export * from './located';
+export * from './located-base';
 export * from './registered';
 export * from './relocated';
+export * from './relocated-base';
 export * from './removed';
 export * from './updated';
+
