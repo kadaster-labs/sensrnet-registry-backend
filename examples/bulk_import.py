84638d6f22ef6ffd4daf4e9f840db642d89a6f7e
Wim Florijn <53855759+kad-floriw@users.noreply.github.com>
2021-03-29 21:12:54 +0200

    Legal-entity events over websocket, fix bulk-import (#98)
    * communicate legal-entity events over websocket
    
    * fix bulk-import script

--- examples/bulk_import.py
+++ examples/bulk_import.py
@@ -2,23 +2,20 @@ import requests
 import pandas as pd
 
 USERNAME, PASSWORD = 'my@email.com', 'my-password'
+URL = 'http://localhost:3000'
 
-response = requests.post('http://127.0.0.1:4200/api/auth/login', {'username': USERNAME, 'password': PASSWORD})
+response = requests.post('{}/api/auth/login'.format(URL), {'username': USERNAME, 'password': PASSWORD})
 t = response.json()['accessToken']
 
-for _, row in pd.read_csv('sensors.csv', delimiter='\t').iterrows():
+for _, row in pd.read_csv('devices.csv', delimiter=';').iterrows():
     data = {
-        'category': row['Category'],
-        'typeName': row['Type'],
         'name': row['Name'],
-        'active': row['Active'],
+        'category': row['Category'],
         'description': row['Description'], 'dataStreams': [],
         'location': {
-            'latitude': row['Latitude'],
-            'longitude': row['Longitude'],
-            'height': row['Height']
+            'location': [row['Longitude'], row['Latitude'], row['Height']],
         }
     }
 
-    r = requests.post('http://127.0.0.1:4200/api/sensor', json=data, headers={'Authorization': 'Bearer {}'.format(t)})
-    print('Registered {}: received ID: {}.'.format(row['Name'], r.json()['sensorId']))
+    r = requests.post('{}/api/device'.format(URL), json=data, headers={'Authorization': 'Bearer {}'.format(t)})
+    print('Registered {}: received ID: {}.'.format(row['Name'], r.json()['deviceId']))
