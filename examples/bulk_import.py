03c580f9a6bc3220b17bb7fa935d1a5d90e6bfad
Wim Florijn <wim.florijn@kadaster.nl>
2021-03-29 16:15:12 +0200

    fix bulk-import script

--- examples/bulk_import.py
+++ examples/bulk_import.py
@@ -2,24 +2,20 @@ import requests
 import pandas as pd
 
 USERNAME, PASSWORD = 'my@email.com', 'my-password'
-URL = 'http://gemeente-a-sensrnet.westeurope.cloudapp.azure.com'
+URL = 'http://localhost:3000'
 
 response = requests.post('{}/api/auth/login'.format(URL), {'username': USERNAME, 'password': PASSWORD})
 t = response.json()['accessToken']
 
-for _, row in pd.read_csv('sensors.csv', delimiter=';').iterrows():
+for _, row in pd.read_csv('devices.csv', delimiter=';').iterrows():
     data = {
-        'category': row['Category'],
-        'typeName': row['Type'],
         'name': row['Name'],
-        'active': row['Active'],
+        'category': row['Category'],
         'description': row['Description'], 'dataStreams': [],
         'location': {
-            'latitude': row['Latitude'],
-            'longitude': row['Longitude'],
-            'height': row['Height']
+            'location': [row['Longitude'], row['Latitude'], row['Height']],
         }
     }
 
-    r = requests.post('{}/api/sensor'.format(URL), json=data, headers={'Authorization': 'Bearer {}'.format(t)})
-    print('Registered {}: received ID: {}.'.format(row['Name'], r.json()['sensorId']))
+    r = requests.post('{}/api/device'.format(URL), json=data, headers={'Authorization': 'Bearer {}'.format(t)})
+    print('Registered {}: received ID: {}.'.format(row['Name'], r.json()['deviceId']))
