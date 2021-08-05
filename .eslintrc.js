4da7a6ba9791b32ba92adbd27fee2241cb5ed19c
Stefan Bussemaker <Stefan.Bussemaker@kadaster.nl>
2021-08-05 20:56:25 +0200

    set a more prescriptive eslint for better consistency

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,10 +6,12 @@ module.exports = {
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-    'prettier/@typescript-eslint',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    "plugin:prettier/recommended",
+    "prettier/@typescript-eslint"
   ],
   root: true,
   env: {
@@ -19,6 +21,13 @@ module.exports = {
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'import/order': ['error', {
+      alphabetize: {
+        order: 'asc',
+        caseInsensitive: true
+      },
+    }],
   },
 };
