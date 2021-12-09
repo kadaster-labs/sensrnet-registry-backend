87b12bf45a203efe30563df9d14e8de6b35d37d7
Stefan Bussemaker <Stefan.Bussemaker@kadaster.nl>
2021-12-09 10:14:47 +0100

    refactor: validate geoJSON type

--- src/command/api/model/data-stream/observed-area.body.ts
+++ src/command/api/model/data-stream/observed-area.body.ts
@@ -1,15 +1,20 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString, IsArray } from 'class-validator';
+import { IsNotEmpty, IsString, IsEnum, IsArray } from 'class-validator';
+
+export enum GeoJSONType {
+    polygon = 'Polygon',
+}
 
 export class ObservedAreaBody {
     @IsString()
     @IsNotEmpty()
+    @IsEnum(GeoJSONType)
     @ApiProperty({
         type: String,
         required: true,
         description: 'GeoJSON Object Type.',
     })
-    readonly type: string;
+    readonly type: GeoJSONType;
 
     @IsArray()
     @IsNotEmpty()
